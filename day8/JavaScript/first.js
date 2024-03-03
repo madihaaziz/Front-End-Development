4b7fc1508e8677cf6bbecf32416ec6e6089951fc
madihaaziz <madiha.aziz.08@gmail.com>
2024-03-04 05:17:31 +0530

    update day 8

---
+++ day8/JavaScript/first.js
@@ -0,0 +1,30 @@
+// console.log("Madiha Aziz");
+// console.log("Welcome to JS day1");
+
+// name="Madiha Aziz";
+// age=21;
+// x=null;
+// y=undefined;
+// console.log(x);
+// console.log(y);
+// let age=24;
+
+// const student={
+//     fullName:"Madiha Aziz",
+//     age:21,
+//     cgpa:8.8,
+//     isPass:true,
+// };
+
+// student["age"]=student["age"]+1;
+
+// console.log(student.age);
+
+const  product={
+    name:"Ball Pen",
+    rating:4,
+    offer:5,
+    price:270,
+    isDeal:true,
+};
+console.log(product)
