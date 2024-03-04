57718e81804f41fd46c99ef66500d842c79a5076
madihaaziz <madiha.aziz.08@gmail.com>
2024-03-05 04:44:14 +0530

    day9

---
+++ JavaScript/day9/script.js
@@ -0,0 +1,74 @@
+//Arithmetic Operator
+
+// let a=5;
+// let b=2;
+
+// console.log("a = ",a ," and b = ",b);
+// console.log("a + b = ",a+b);
+// console.log("a - b = ",a-b);
+// console.log("a * b = ",a*b);
+// console.log("a / b = ",a/b);
+// console.log("a % b = ",a%b);
+// console.log("a ** b = ",a**b);
+
+// let a=5;
+// let b=2;
+// let c=4;
+// let d=6;
+// //Unary Operator
+// console.log("a = ",a ," and b = ",b);
+//a+=1;
+//console.log("a = ",a);
+//console.log("a++ = ",a++);
+//console.log("a = ",a);
+//b-=1;
+//console.log("b = ",b);
+
+//Assignment Operator
+
+// a+=4;
+// console.log(a);
+// b*=2;
+// console.log(b);
+// c-=2;
+// console.log(c);
+// d%=3;
+// console.log(d);
+
+
+let age=16;
+// if (age>=18){
+//     console.log("Can Vote");
+// }
+// else{
+//     console.log("Can't Vote");
+// }
+
+// let res=age>=18?"adult":"not adult";
+// console.log(res);
+
+//get a user input and check if it's a multiple of 5
+
+let score=prompt("Enter the score: ");
+// if(num%5==0){
+//     console.log("Divisible by 5");
+// }
+// else{
+//     console.log("Not divisible by 5");
+// }
+
+if(score>=80 && score<=100){
+    console.log("A");
+}
+else if(score>=70 && score<=79){
+    console.log("B");
+}
+else if(score>=60 && score<=69){
+    console.log("C");
+}
+else if(score>=50 && score<=599){
+    console.log("D");
+}
+else {
+    console.log("F");
+}
